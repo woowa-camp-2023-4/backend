6371bbc3fa7757d05de502e742c04e941a90768a
chanwooDev <pove2019@gmail.com>
2023-08-13 14:09:57 +0900

    feat: init project

---
+++ src/main/java/com/woowa/woowakit/WoowakitApplication.java
@@ -0,0 +1,13 @@
+package com.woowa.woowakit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoowakitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoowakitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woowa/woowakit/WoowakitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woowa.woowakit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoowakitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
