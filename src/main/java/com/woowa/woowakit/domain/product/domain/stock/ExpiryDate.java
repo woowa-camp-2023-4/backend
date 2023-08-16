b3aea56a8df95631a2007ddbc0122dc3b2c3a6ec
bkRyusim <bkryusim@gmail.com>
2023-08-16 21:29:01 +0900

    feat: 유통기한 관련 검증 로직 테스트 및 구현

--- src/main/java/com/woowa/woowakit/domain/product/domain/stock/ExpiryDate.java
+++ src/main/java/com/woowa/woowakit/domain/product/domain/stock/ExpiryDate.java
@@ -2,6 +2,8 @@ package com.woowa.woowakit.domain.product.domain.stock;
 
 import java.time.LocalDate;
 
+import com.woowa.woowakit.domain.product.exception.StockExpiredException;
+
 import lombok.Getter;
 
 @Getter
@@ -10,10 +12,17 @@ public class ExpiryDate {
 	private final LocalDate date;
 
 	private ExpiryDate(final LocalDate date) {
+		validExpiryDate(date);
 		this.date = date;
 	}
 
 	public static ExpiryDate from(final LocalDate date) {
 		return new ExpiryDate(date);
+	}
+
+	private void validExpiryDate(final LocalDate date) {
+		if (date.isBefore(LocalDate.now())) {
+			throw new StockExpiredException();
+		}
 	}
 }
