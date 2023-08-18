c10b685ed75510a3b58850e360bd4ea036986791
bkRyusim <bkryusim@gmail.com>
2023-08-18 21:06:03 +0900

    fix: 프로젝트 디렉터리 변경

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-PROJECT_ROOT="$HOME/backend"
-JAR_FILE=build/libs/$(ls build/libs/*.jar*)
+JAR_FILE=$(ls build/libs/*.jar*)
 
 APP_LOG="application.log"
 ERROR_LOG="error.log"
 DEPLOY_LOG="deploy.log"
 
 TIME_NOW=$(date + %c)
+
+chmod 755 $JAR_FILE
 
 echo "[ $TIME_NOW ] Run java application $JAR_FILE" >>$DEPLOY_LOG
 nohup java -jar $JAR_FILE >$APP_LOG 2>$ERROR_LOG &
