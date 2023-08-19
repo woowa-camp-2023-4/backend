167e9d818c9e06a8ded8d21be270bec635116ace
impati <impati0716@icloud.com>
2023-08-19 20:00:04 +0900

    feat: CartItemAddRequest valid 에 메시지 추가

--- src/main/java/com/woowa/woowakit/domain/cart/dto/CartItemAddRequest.java
+++ src/main/java/com/woowa/woowakit/domain/cart/dto/CartItemAddRequest.java
@@ -13,10 +13,10 @@ import javax.validation.constraints.NotNull;
 @Getter
 public class CartItemAddRequest {
 
-    @NotNull
+    @NotNull(message = "상품 ID 는 필수입니다.")
     private Long productId;
 
-    @Min(value = 1)
+    @Min(value = 1, message = "상품 수량은 1개이상이어야 합니다.")
     private long quantity;
 
     public static CartItemAddRequest of(final Long productId, final Long quantity) {
