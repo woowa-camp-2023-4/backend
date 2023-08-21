5936be118a9dca3c6d1185532b43c99be9a29af0
bkRyusim <bkryusim@gmail.com>
2023-08-21 12:36:39 +0900

    feat: 결제 정보 추가

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,12 +6,12 @@ APP_LOG="application.log"
 ERROR_LOG="error.log"
 DEPLOY_LOG="deploy.log"
 
-TIME_NOW=$(date + %c)
+TIME_NOW=$(date)
 
 chmod 755 $JAR_FILE
 
 echo "[ $TIME_NOW ] Run java application $JAR_FILE" >>$DEPLOY_LOG
-nohup java -jar $JAR_FILE >$APP_LOG 2>$ERROR_LOG &
+nohup java -jar $JAR_FILE -Dspring.profiles.active=local >$APP_LOG 2>$ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "[ $TIME_NOW ] Application running in PID $CURRENT_PID" >>$DEPLOY_LOG
