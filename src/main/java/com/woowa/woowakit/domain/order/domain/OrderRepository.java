cbca443b6c36161a39b6a5f05b3e4ca997ddeb46
jiwon choi <jjiwoning12@gmail.com>
2023-08-21 17:12:01 +0900

    feat: 주문 조회 쿼리 생성 및 테스트

--- src/main/java/com/woowa/woowakit/domain/order/domain/OrderRepository.java
+++ src/main/java/com/woowa/woowakit/domain/order/domain/OrderRepository.java
@@ -1,7 +1,17 @@
 package com.woowa.woowakit.domain.order.domain;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
+	@Query("select o from Order o join fetch o.orderItems where o.memberId = :memberId")
+	List<Order> findAllOrdersJoinOrderItemsByMemberId(@Param("memberId") Long memberId);
+
+	@Query("select o from Order o join fetch o.orderItems where o.id = :id and o.memberId = :memberId")
+	Optional<Order> findOrderJoinOrderItemsById(@Param("id") Long id, @Param("memberId") Long memberId);
 }
---
+++ src/test/java/com/woowa/woowakit/domain/order/dao/OrderRepositoryTest.java
@@ -0,0 +1,119 @@
+package com.woowa.woowakit.domain.order.dao;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import com.woowa.woowakit.domain.auth.domain.EncodedPassword;
+import com.woowa.woowakit.domain.auth.domain.Member;
+import com.woowa.woowakit.domain.model.Image;
+import com.woowa.woowakit.domain.model.Money;
+import com.woowa.woowakit.domain.model.Quantity;
+import com.woowa.woowakit.domain.order.domain.Order;
+import com.woowa.woowakit.domain.order.domain.OrderItem;
+import com.woowa.woowakit.domain.order.domain.OrderRepository;
+import com.woowa.woowakit.domain.order.fixture.OrderFixture;
+import com.woowa.woowakit.global.config.QuerydslTestConfig;
+
+@DisplayName("OrderRepository 단위 테스트")
+@DataJpaTest
+@Import(QuerydslTestConfig.class)
+class OrderRepositoryTest {
+
+	@Autowired
+	private EntityManager entityManager;
+
+	@Autowired
+	private OrderRepository orderRepository;
+
+	@Test
+	@DisplayName("회원의 주문 상세 정보를 확인한다.")
+	void findById() {
+		// given
+		Member member = Member.of("tamtam@hello.com", EncodedPassword.from("happytamtam"), "탐탐");
+		entityManager.persist(member);
+
+		OrderItem orderItem1 = OrderFixture.anOrderItem()
+			.productId(1L)
+			.name("강아지가 좋아하는 캥거루 꼬리 밀키트")
+			.image(Image.from("dogFood.img"))
+			.price(Money.from(10000L))
+			.quantity(Quantity.from(5L))
+			.build();
+
+		OrderItem orderItem2 = OrderFixture.anOrderItem()
+			.productId(2L)
+			.name("알러지 없는 강아지 밀키트 모음")
+			.image(Image.from("dogFood2.img"))
+			.price(Money.from(70000L))
+			.quantity(Quantity.from(4L))
+			.build();
+
+		Order order = OrderFixture.anOrder()
+			.memberId(member.getId())
+			.orderItems(List.of(orderItem1, orderItem2))
+			.build();
+
+		Long orderId = orderRepository.save(order).getId();
+
+		// when
+		Order result = orderRepository.findOrderJoinOrderItemsById(orderId, member.getId()).get();
+
+		// then
+		assertThat(result).extracting(Order::getId).isEqualTo(orderId);
+		assertThat(result.getOrderItems()).hasSize(2);
+		assertThat(result.getOrderItems().get(0)).extracting("id").isEqualTo(orderItem1.getId());
+		assertThat(result.getOrderItems().get(1)).extracting("id").isEqualTo(orderItem2.getId());
+	}
+
+	@Test
+	@DisplayName("회원의 주문 정보들을 확인한다.")
+	void findAllByMemberId() {
+		// given
+		Member member = Member.of("tamtam@hello.com", EncodedPassword.from("happytamtam"), "탐탐");
+		entityManager.persist(member);
+
+		OrderItem orderItem1 = OrderFixture.anOrderItem()
+			.productId(1L)
+			.name("강아지가 좋아하는 캥거루 꼬리 밀키트")
+			.image(Image.from("dogFood.img"))
+			.price(Money.from(10000L))
+			.quantity(Quantity.from(5L))
+			.build();
+
+		OrderItem orderItem2 = OrderFixture.anOrderItem()
+			.productId(2L)
+			.name("알러지 없는 강아지 밀키트 모음")
+			.image(Image.from("dogFood2.img"))
+			.price(Money.from(70000L))
+			.quantity(Quantity.from(4L))
+			.build();
+
+		Order order1 = OrderFixture.anOrder()
+			.memberId(member.getId())
+			.orderItems(List.of(orderItem1))
+			.build();
+
+		Order order2 = OrderFixture.anOrder()
+			.memberId(member.getId())
+			.orderItems(List.of(orderItem2))
+			.build();
+
+		orderRepository.save(order1);
+		orderRepository.save(order2);
+
+		// when
+		List<Order> result = orderRepository.findAllOrdersJoinOrderItemsByMemberId(member.getId());
+
+		// then
+		assertThat(result).hasSize(2);
+	}
+}
