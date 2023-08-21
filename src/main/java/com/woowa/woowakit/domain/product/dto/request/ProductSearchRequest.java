1bed02b5b6ddc321cf6e1e6a7fffa4a55f64d9a2
jiwon choi <jjiwoning12@gmail.com>
2023-08-21 14:57:28 +0900

    refactor: Validation 추가

--- src/main/java/com/woowa/woowakit/domain/product/dto/request/ProductSearchRequest.java
+++ src/main/java/com/woowa/woowakit/domain/product/dto/request/ProductSearchRequest.java
@@ -1,6 +1,7 @@
 package com.woowa.woowakit.domain.product.dto.request;
 
 import javax.validation.constraints.Min;
+import javax.validation.constraints.Null;
 
 import com.woowa.woowakit.domain.product.domain.product.ProductSearchCondition;
 
@@ -16,8 +17,10 @@ import lombok.Setter;
 @Setter
 public class ProductSearchRequest {
 
+	@Null
 	private String productKeyword;
 
+	@Null
 	private Long lastProductId;
 
 	@Min(value = 1, message = "최소 1개 이상의 상품을 조회해야합니다.")
