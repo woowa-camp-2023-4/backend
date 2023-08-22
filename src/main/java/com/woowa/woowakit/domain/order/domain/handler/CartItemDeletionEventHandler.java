589f7b78a32e555a92a46ec0afdf331b9513e837
jiwon choi <jjiwoning12@gmail.com>
2023-08-22 16:44:26 +0900

    refactor: CartItemDeletionEventHandler 네이밍 수정 및 이벤트 순서 제거

--- src/main/java/com/woowa/woowakit/domain/order/domain/handler/CartItemDeletionEventHandler.java
+++ src/main/java/com/woowa/woowakit/domain/order/domain/handler/CartItemDeletionEventHandler.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.context.event.EventListener;
-import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,11 +15,10 @@ import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
-public class CartItemDeleteEventHandler {
+public class CartItemDeletionEventHandler {
 
 	private final CartItemRepository cartItemRepository;
 
-	@Order(1)
 	@Transactional
 	@EventListener
 	public void handle(final OrderCompleteEvent event) {
