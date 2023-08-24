182932483e965db31a9b5bd3ce0a0ef3bf5cb9e2
chanwooDev <pove2019@gmail.com>
2023-08-24 18:54:43 +0900

    fix: 가우스 함수 인자 없는 것으로 변경
    ci에서 오류가 나서 변경함

--- src/main/java/com/woowa/woowakit/infra/payment/toss/ThreadSleepClient.java
+++ src/main/java/com/woowa/woowakit/infra/payment/toss/ThreadSleepClient.java
@@ -8,14 +8,14 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ThreadSleepClient implements PaymentService {
 
-	public static final double MEAN = 1.3;
-	public static final double STANDARD_DEVIATION = 0.2;
-	private final Random random = new Random();
+	private static final double MEAN = 1.3;
+	private static final double STANDARD_DEVIATION = 0.2;
 
 	@Override
 	public void validatePayment(String paymentKey, String orderToken, Money totalPrice) {
 		try {
-			long latancyMs = (long) (random.nextGaussian(MEAN, STANDARD_DEVIATION) * 1000);
+			long latancyMs = (long) ((MEAN + STANDARD_DEVIATION * new Random().nextGaussian())
+				* 1000);
 			log.info("결제를 위해 {} ms 대기합니다.", latancyMs);
 			Thread.sleep(latancyMs);
 		} catch (InterruptedException e) {
