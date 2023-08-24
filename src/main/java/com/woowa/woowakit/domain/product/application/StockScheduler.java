508617a640c93f8f9338d38d0547418b991d814d
최준영 <75917903+impati@users.noreply.github.com>
2023-08-24 21:50:31 +0900

    재고 정합성 스케쥴러 로깅 추가 (#90)
    * feat: StockScheduler 로깅
    
    * feat: StockScheduler 로깅
    
    * feat: StockScheduler productId 추가

--- src/main/java/com/woowa/woowakit/domain/product/application/StockScheduler.java
+++ src/main/java/com/woowa/woowakit/domain/product/application/StockScheduler.java
@@ -1,6 +1,7 @@
 package com.woowa.woowakit.domain.product.application;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.List;
 
@@ -22,10 +23,16 @@ public class StockScheduler {
 
 	@Scheduled(zone = "Asia/Seoul", cron = "0 0 0 * * ?")
 	public void trigger() {
+		long startEnd = System.currentTimeMillis();
+		log.info("재고 정합성 스케쥴러 시작 = {}", LocalDateTime.now());
 		List<Long> productIds = productRepository.findAllIds();
 
 		for (Long productId : productIds) {
+			log.info("상품 정합성 시작 = {} productId = {}", LocalDateTime.now(), productId);
 			stockProcessingService.doProcess(productId, LocalDate.now(ZoneId.of("Asia/Seoul")));
+			log.info("상품 정합성 끝 = {}", LocalDateTime.now());
 		}
+		long diffTime = System.currentTimeMillis() - startEnd;
+		log.info("재고 정합성 스케쥴러 끝 = {} , 걸린 시간 = {} ms ", LocalDateTime.now(), diffTime);
 	}
 }
