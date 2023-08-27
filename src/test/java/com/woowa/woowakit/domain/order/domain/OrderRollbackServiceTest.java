360d1dfab8b6d7cd52c54503ea0caee33316002b
bkRyusim <bkryusim@gmail.com>
2023-08-27 21:18:45 +0900

    test: OrderRollbackService의 롤백 로직 테스트

---
+++ src/test/java/com/woowa/woowakit/domain/order/domain/OrderRollbackServiceTest.java
@@ -0,0 +1,35 @@
+package com.woowa.woowakit.domain.order.domain;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.woowa.woowakit.domain.cart.domain.CartItemRepository;
+import com.woowa.woowakit.domain.model.Quantity;
+import com.woowa.woowakit.domain.order.fixture.OrderFixture;
+import com.woowa.woowakit.domain.product.domain.product.Product;
+import com.woowa.woowakit.domain.product.domain.product.ProductRepository;
+import com.woowa.woowakit.domain.product.fixture.ProductFixture;
+
+class OrderRollbackServiceTest {
+
+	@Test
+	@DisplayName("rollback 시 담은 개수만큼 다시 복구되는지 테스트.")
+	void rollback() {
+		// given
+		OrderRollbackService orderRollbackService = new OrderRollbackService(mock(CartItemRepository.class), mock(
+			ProductRepository.class), mock(CartItemMapper.class));
+		Order order = OrderFixture.anOrder().build();
+		Product product = ProductFixture.anProduct().id(2L).quantity(Quantity.from(1L)).build();
+
+		// when
+		orderRollbackService.rollbackProducts(order, List.of(product));
+
+		// then
+		assertThat(product).extracting("quantity").extracting("value").isEqualTo(2L);
+	}
+}
