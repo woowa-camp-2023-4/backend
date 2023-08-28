0dfeb63aaebc46ab7d0f692ced9967d1d81c8f51
chanwooDev <pove2019@gmail.com>
2023-08-28 10:12:04 +0900

    refactor: ThreadSleepClient publishOn 삭제

--- src/main/java/com/woowa/woowakit/infra/payment/toss/ThreadSleepClient.java
+++ src/main/java/com/woowa/woowakit/infra/payment/toss/ThreadSleepClient.java
@@ -1,11 +1,9 @@
 package com.woowa.woowakit.infra.payment.toss;
 
-import java.time.Duration;
-import java.util.Random;
-
 import com.woowa.woowakit.domain.model.Money;
 import com.woowa.woowakit.domain.payment.domain.PaymentClient;
-
+import java.time.Duration;
+import java.util.Random;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
@@ -27,12 +25,12 @@ public class ThreadSleepClient implements PaymentClient {
 		final Money totalPrice
 	) {
 		long latancyMs =
-			(long)((LATENCY_MIN + STANDARD_DEVIATION * new Random().nextGaussian()) * 1000);
+			(long) ((LATENCY_MIN + STANDARD_DEVIATION * new Random().nextGaussian()) * 1000);
 		log.info("결제 요청 반환에 {} ms 가 수행됩니다. paymentKey: {}", latancyMs, paymentKey);
 
-		return Mono.delay(Duration.ofMillis(latancyMs))
-			.publishOn(scheduler)
-			.log("[mono]mono delay에 " + latancyMs + " ms가 수행되었습니다. paymentKey: " + paymentKey)
-			.then();
+		return
+			Mono.delay(Duration.ofMillis(latancyMs))
+				.log("[mono]mono delay에 " + latancyMs + " ms가 수행되었습니다. paymentKey: " + paymentKey)
+				.then();
 	}
 }
