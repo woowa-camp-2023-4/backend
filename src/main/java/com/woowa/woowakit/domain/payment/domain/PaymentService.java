10733f0a1059816dc0953bff249433e3159ebdfe
chanwooDev <pove2019@gmail.com>
2023-08-28 10:14:47 +0900

    feat: 메트릭 추가

--- src/main/java/com/woowa/woowakit/domain/payment/domain/PaymentService.java
+++ src/main/java/com/woowa/woowakit/domain/payment/domain/PaymentService.java
@@ -1,17 +1,15 @@
 package com.woowa.woowakit.domain.payment.domain;
 
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.woowa.woowakit.domain.order.domain.Order;
 import com.woowa.woowakit.domain.order.domain.OrderRepository;
 import com.woowa.woowakit.domain.order.domain.OrderRollbackService;
 import com.woowa.woowakit.domain.order.domain.event.OrderCompleteEvent;
-
 import io.micrometer.core.annotation.Counted;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -24,7 +22,7 @@ public class PaymentService {
 	private final OrderRollbackService orderRollbackService;
 
 	@Transactional(propagation = Propagation.REQUIRES_NEW)
-	@Counted("order.payment.async")
+	@Counted("order.payment.success")
 	public void handlePaySuccess(final OrderCompleteEvent event) {
 		Order order = findOrderById(event.getOrder().getId());
 		Payment payment = paymentMapper.mapFrom(event);
@@ -35,8 +33,10 @@ public class PaymentService {
 	}
 
 	@Transactional(propagation = Propagation.REQUIRES_NEW)
+	@Counted("order.payment.failure")
 	public void handlePayError(final OrderCompleteEvent event, final Throwable error) {
-		log.error("결제 실패 복구 시작 paymentKey: {}, message={}", event.getPaymentKey(), error.getMessage());
+		log.error("결제 실패 복구 시작 paymentKey: {}, message={}", event.getPaymentKey(),
+			error.getMessage());
 		Order order = findOrderById(event.getOrder().getId());
 		order.rollback(orderRollbackService);
 	}
