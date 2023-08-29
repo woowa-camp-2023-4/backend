72bacd385da17cd06880e9cf5668f97db9adbecc
bkRyusim <bkryusim@gmail.com>
2023-08-29 10:26:15 +0900

    refactor: AbstractAggregateRoot 제거

--- src/main/java/com/woowa/woowakit/domain/model/BaseEntity.java
+++ src/main/java/com/woowa/woowakit/domain/model/BaseEntity.java
@@ -7,7 +7,6 @@ import javax.persistence.MappedSuperclass;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.domain.AbstractAggregateRoot;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import lombok.Getter;
@@ -15,7 +14,7 @@ import lombok.Getter;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 @Getter
-public abstract class BaseEntity extends AbstractAggregateRoot<BaseEntity> {
+public abstract class BaseEntity {
 
 	@CreatedDate
 	private LocalDateTime createdAt;
