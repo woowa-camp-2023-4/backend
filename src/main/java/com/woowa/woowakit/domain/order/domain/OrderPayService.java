fcc8ef20d429f1943b6402d1c75018de10d23ce5
chanwooDev <pove2019@gmail.com>
2023-08-29 21:48:33 +0900

    feat: 비동기 스레드 풀 추가

--- src/main/java/com/woowa/woowakit/domain/order/domain/OrderPayService.java
+++ src/main/java/com/woowa/woowakit/domain/order/domain/OrderPayService.java
@@ -7,6 +7,7 @@ import io.micrometer.core.annotation.Counted;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 
 @Slf4j
@@ -17,6 +18,8 @@ public class OrderPayService {
 	private final PaymentClient paymentClient;
 	private final OrderRepository orderRepository;
 	private final PayResultHandler payResultHandler;
+	private final Scheduler boundedElasticScheduler = Schedulers.newBoundedElastic(100,
+		Integer.MAX_VALUE, "order.pay");
 
 	@Counted("order.payment.request")
 	public void pay(final Long orderId, final String paymentKey) {
@@ -25,7 +28,7 @@ public class OrderPayService {
 		Order order = orderRepository.findById(orderId).orElseThrow(OrderNotFoundException::new);
 
 		paymentClient.validatePayment(paymentKey, order.getUuid(), order.getTotalPrice())
-			.publishOn(Schedulers.boundedElastic())
+			.publishOn(boundedElasticScheduler)
 			.doOnSuccess(ignore -> payResultHandler.save(orderId, paymentKey))
 			.doOnError(error -> payResultHandler.rollback(orderId, error))
 			.onErrorMap(IllegalArgumentException.class, InvalidPayRequestException::new)
